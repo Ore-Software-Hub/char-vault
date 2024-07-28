4b5689b61f3652e0ec8d8bf1bc4c7f38b30b98b8
Pedrozle <pedrozle@outlook.com>
2024-07-28 14:59:10 -0400

    criado tela perfil

---
+++ lib/pages/profile/components/details_component.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class DetailsComponent extends StatefulWidget {
+  const DetailsComponent({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  final String title;
+  final String value;
+
+  @override
+  State<DetailsComponent> createState() => _DetailsComponentState();
+}
+
+class _DetailsComponentState extends State<DetailsComponent> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      child: Column(
+        children: [
+          Text(
+            widget.value,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          const Divider(),
+          Text(
+            widget.title,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+            softWrap: false,
+            style: const TextStyle(
+              fontWeight: FontWeight.w300,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/profile/profile_page.dart
@@ -0,0 +1,322 @@
+import 'package:character_vault/pages/profile/components/details_component.dart';
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String name = "Garry Floyd",
+      classe = "Bardo",
+      level = "1",
+      curLife = "15",
+      maxLife = "17",
+      pg = "15",
+      pp = "15",
+      pb = "15";
+
+  @override
+  void initState() {
+    super.initState();
+    // isUser = Provider.of<LoginProvider>(context, listen: false).userLogged;
+    // if (isUser == null) {
+    //   Navigator.pop(context);
+    // }
+    // loadUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              clipBehavior: Clip.none,
+              children: [
+                // Gradiente de fundo
+                Container(
+                  height: (MediaQuery.of(context).size.height / 2),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    gradient: RadialGradient(
+                      colors: [
+                        Color.fromARGB(255, 155, 165, 149),
+                        Color.fromARGB(255, 85, 87, 84),
+                      ],
+                      center: Alignment.center,
+                      radius: 0.5,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 400,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    "assets/img/char.png",
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  child: Container(
+                    height: 130,
+                    width: MediaQuery.of(context).size.width,
+                    decoration:
+                        BoxDecoration(color: Colors.black.withAlpha(150)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 16, right: 16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                name,
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                              Text(
+                                classe,
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w100,
+                                ),
+                              ),
+                              Row(
+                                children: [
+                                  Row(
+                                    children: [
+                                      const PhosphorIcon(
+                                        PhosphorIconsRegular.heart,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        curLife,
+                                        style: const TextStyle(
+                                          fontSize: 24,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        maxLife,
+                                        style: const TextStyle(
+                                          fontSize: 24,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w100,
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: Row(
+                                          children: [
+                                            const PhosphorIcon(
+                                                PhosphorIconsRegular.coin,
+                                                color: Colors.amber),
+                                            Text(
+                                              pg,
+                                              style: const TextStyle(
+                                                fontSize: 24,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: Row(
+                                          children: [
+                                            const PhosphorIcon(
+                                                PhosphorIconsRegular.coin,
+                                                color: Colors.white70),
+                                            Text(
+                                              pp,
+                                              style: const TextStyle(
+                                                fontSize: 24,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(right: 8.0),
+                                        child: Row(
+                                          children: [
+                                            const PhosphorIcon(
+                                                PhosphorIconsRegular.coin,
+                                                color: Color.fromARGB(
+                                                    255, 189, 86, 49)),
+                                            Text(
+                                              pb,
+                                              style: const TextStyle(
+                                                fontSize: 24,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w300,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const Spacer(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, bottom: 8),
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 80,
+                                  alignment: Alignment.center,
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Colors.transparent, // Sem preenchimento
+                                    borderRadius: BorderRadius.circular(
+                                        15.0), // Raio da borda
+                                    border: Border.all(
+                                      color: Colors.amber, // Cor da borda
+                                      width: 2.0, // Largura da borda
+                                    ),
+                                  ),
+                                  child: Text(
+                                    level,
+                                    style: const TextStyle(
+                                      fontSize: 45,
+                                      color: Colors.amber,
+                                      fontWeight: FontWeight.w900,
+                                    ),
+                                  ),
+                                ),
+                                const Text(
+                                  "Nível",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w100,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 16,
+                  left: 16,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: ClipRRect(
+                          borderRadius: BorderRadius.circular(
+                              10.0), // Adjust the radius as needed
+                          child: Image.asset(
+                            'assets/img/eu.jpg',
+                            width: 50,
+                          ),
+                        ),
+                        onPressed: () {
+                          Scaffold.of(context)
+                              .openDrawer(); // Abrir o Drawer ao clicar no ícone de menu
+                        },
+                      ),
+                      const Text(
+                        "João Pedro",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                    ],
+                  ),
+                ),
+                // Imagem posicionada ao lado
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 8, left: 16, right: 16, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Sobre",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      DetailsComponent(title: "Idade", value: "26 anos"),
+                      DetailsComponent(title: "Raça", value: "Humano"),
+                      DetailsComponent(title: "Antecedentes", value: "Artista"),
+                      DetailsComponent(title: "Alinhamento", value: "Neutro"),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 16, right: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "História",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent, // Sem preenchimento
+                      borderRadius: BorderRadius.circular(8.0), // Raio da borda
+                      border: Border.all(
+                        color: Colors.black, // Cor da borda
+                        width: 1.0, // Largura da borda
+                      ),
+                    ),
+                    child: const Text(
+                        "Garry é um músico que não tem talento para tocar alaúde, ele entrou na escola de música depois de ter se apaixonado na professora de música. Passou 1 ano na escola apenas flertando com a professora e não aprendendo nada. Seus pais pagaram 1 ano de aula e esperavam retorno, que nunca veio. Garry então mentiu para seus pais dizendo que ele teria uma turnê pelo mundo e precisava viajar por um tempo, mas era apenas uma desculpa esfarrapada para vadiar pelo mundo azarando as mulheres e torrando a heraça dele e de seus irmãos."),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

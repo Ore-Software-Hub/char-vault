953d21f6cfa48ce2c1c20899acceb750098ab058
Pedrozle <pedrozle@outlook.com>
2024-07-29 14:06:12 -0400

    adicionado tela landing page e login

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,59 @@
+import 'package:character_vault/components/button/button_component.dart';
+import 'package:character_vault/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+    super.initState();
+    // isUser = Provider.of<LoginProvider>(context, listen: false).userLogged;
+    // if (isUser == null) {
+    //   Navigator.pop(context);
+    // }
+    // loadUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 50, bottom: 50, left: 16, right: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Vamos jogar RPG?",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: ButtonComponent(
+                  pressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                  },
+                  label: "Começar",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,59 @@
+import 'package:character_vault/components/button/button_component.dart';
+import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    super.initState();
+    // isUser = Provider.of<LoginProvider>(context, listen: false).userLogged;
+    // if (isUser == null) {
+    //   Navigator.pop(context);
+    // }
+    // loadUser();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 50, bottom: 50, left: 16, right: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Vamos jogar RPG?",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text("Escolha como fazer login abaixo"),
+              Column(
+                children: [
+                  SizedBox(
+                    child: ButtonComponent(
+                        pressed: () {},
+                        icon: PhosphorIconsBold.googleLogo,
+                        label: "Entrar com Google",
+                        tipo: 1,
+                        color: Colors.blue),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

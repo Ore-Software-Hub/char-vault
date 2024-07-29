d7e4a4e3c5ea4e4aa4c7e5c9b1c2685320a07f60
Pedrozle <pedrozle@outlook.com>
2024-07-29 15:02:52 -0400

    criado textfield comp

---
+++ lib/components/text_field_component.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class TextFieldComponent extends StatefulWidget {
+  const TextFieldComponent({super.key, this.color});
+
+  final Color? color;
+
+  @override
+  State<TextFieldComponent> createState() => _TextFieldComponentState();
+}
+
+class _TextFieldComponentState extends State<TextFieldComponent> {
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  textFieldDecoration(String title) {
+    return InputDecoration(
+      labelText: title,
+      labelStyle: TextStyle(
+          color: widget.color != null
+              ? widget.color!
+              : Theme.of(context)
+                  .colorScheme
+                  .primary), // Cor do rótulo do TextField
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: widget.color != null
+              ? widget.color!
+              : Theme.of(context).colorScheme.primary, // Cor da borda
+        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: widget.color != null
+              ? widget.color!
+              : Theme.of(context)
+                  .colorScheme
+                  .primary, // Cor da borda quando habilitado
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: widget.color != null
+              ? widget.color!
+              : Theme.of(context)
+                  .colorScheme
+                  .primary, // Cor da borda quando focado
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      style: TextStyle(
+        color: widget.color != null
+            ? widget.color!
+            : Theme.of(context).colorScheme.primary,
+      ),
+      cursorColor: widget.color != null
+          ? widget.color!
+          : Theme.of(context).colorScheme.primary,
+      decoration: textFieldDecoration("Nome do Item"),
+    );
+  }
+}

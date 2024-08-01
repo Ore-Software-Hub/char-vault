70ca598f1f218ea6131837db63d912121f33b8f7
Pedrozle <pedrozle@outlook.com>
2024-08-01 10:51:14 -0400

    adicionado documentação

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -3,7 +3,7 @@ import 'package:firebase_database/firebase_database.dart';
 class DatabaseService {
   static final _database = FirebaseDatabase.instance;
 
-  // Método para salvar dados em uma coleção e retornar a chave única gerada
+  /// Método para salvar dados em uma coleção e retornar a chave única gerada
   static Future<String> saveData(String collection, dynamic value) async {
     var ref = _database.ref(collection);
 
@@ -14,7 +14,7 @@ class DatabaseService {
     return newKey;
   }
 
-  // Método para adicionar um personagem a um usuário
+  /// Método para adicionar um personagem a um usuário
   static Future<void> addCharacter(String userId, dynamic newCharacter) async {
     String charId = await saveData('Chars',
         newCharacter); // Salvando o personagem na coleção de personagens
@@ -27,7 +27,7 @@ class DatabaseService {
         .set(true); // Adicionando a referência ao personagem no nó do usuário
   }
 
-// Método para deletar um personagem de um usuário
+  /// Método para deletar um personagem de um usuário
   static Future<void> deleteCharacter(String userId, String charId) async {
     var userCharsRef = _database.ref(
         'Users/$userId/chars/$charId'); // Referência ao nó de personagens do usuário
@@ -41,21 +41,21 @@ class DatabaseService {
     await charRef.remove(); // Removendo o personagem da coleção de personagens
   }
 
-  // Método para ler os dados de um personagem específico
+  /// Método para ler os dados de um personagem específico
   static Future<DataSnapshot> getCharacter(String charId) async {
     var charRef = _database.ref('Chars/$charId');
     DataSnapshot snapshot = await charRef.get();
     return snapshot;
   }
 
-  // Método para atualizar os dados de um personagem específico
+  /// Método para atualizar os dados de um personagem específico
   static Future<void> updateCharacter(
       String charId, Map<String, dynamic> updatedData) async {
     var charRef = _database.ref('Chars/$charId');
     await charRef.update(updatedData);
   }
 
-  // Método para obter todos os personagens de um usuário
+  /// Método para obter todos os personagens de um usuário
   static Future<List<DataSnapshot>> getUserCharacters(String userId) async {
     var userCharsRef = _database.ref('Users/$userId/chars');
     DataSnapshot snapshot = await userCharsRef.get();
